3c7a467236935616081dd60485202056280a655c
leemutai <mutailee361@gmail.com>
2024-12-02 22:57:32 +0300

    using loops to render elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const name = "John Doe";
+  const x = 10;
+  const y = 20;
+  //using loops and rebdering elements/lists
+  const names = ["Brad", "Mary", "John", "Kelly"];
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map(name, (index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default App;
