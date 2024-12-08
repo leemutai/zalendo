3bdaa1093a810de5bdbfef6c19376be2ae21b051
leemutai <mutailee361@gmail.com>
2024-12-08 16:19:16 +0300

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
 const App = () => {
-  const name = "John Doe";
-  const x = 10;
-  const y = 20;
-  //using loops and rebdering elements/lists
-  const names = ["Brad", "Mary", "John", "Kelly"];
-  const loggedIn = true;
-  // if (loggedIn) {
-  //   return <h1>Hello member</h1>;
-  // }
-  return (
-    <>
-      <div className="text-5xl">App</div>
-      <p>Hello {name}</p>
-      <p>
-        The sum of {x} and {y} is {x + y}
-      </p>
-      <ul>
-        {names.map(name, (index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {loggedIn ? <h1>Hello member</h1> : <h1>Hello Guest</h1>}
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
